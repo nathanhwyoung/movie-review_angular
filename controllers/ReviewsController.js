d19872e3335e5a3281271dc5f8eb161e0dbd0955
nana <nathanyoung@Nathans-MacBook-Pro.local>
2015-10-02 16:07:02 -0700

    clean up css

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -1,4 +1,8 @@
 movieReviewSite.controller('ReviewsCtrl', function ReviewsCtrl($scope, $stateParams, ReviewsFactory, UtilitiesFactory) {
     $scope.reviews = ReviewsFactory.reviews;
     $scope.ReviewsFactory = ReviewsFactory;
+
+    if ($stateParams.reviewId) {
+        $scope.selectedReview = UtilitiesFactory.findById(ReviewsFactory.reviews, $stateParams.reviewId);
+    }
 })
